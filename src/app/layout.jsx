20d15da3bc51f66e29576343792db024fb58669c
BE-pdc <peterdeclercq1997@outlook.com>
2023-06-21 13:19:47 +0200

    💄  Add tab title

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,13 +6,12 @@ import { ChakraProvider } from "@chakra-ui/react";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>WoonExpertVlaanderen - CRM</title>
+      </head>
       <body>
         <ChakraProvider>{children}</ChakraProvider>
       </body>
     </html>
   );
 }
-
-export const metadata = {
-  title: "WoonExpertVlaanderen - CRM",
-};
