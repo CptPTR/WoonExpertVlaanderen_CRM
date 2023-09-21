03214cfe87770dc6a58e1a42f2df831b7cbdc9a5
Peter De Clercq <peterdeclercq1997@outlook.com>
2023-09-21 13:06:12 +0200

    💄 Add time to status "Ingepland"

--- src/app/(dashboard)/keuringen/[id]/page.jsx
+++ src/app/(dashboard)/keuringen/[id]/page.jsx
@@ -213,10 +213,11 @@ const Keuring = ({ params }) => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
-    const hours = date.getHours();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
     const amOrPm = hours >= 12 ? "PM" : "AM";
 
-    return `${day}/${month}/${year} ${amOrPm}`;
+    return `${day}/${month}/${year} ${hours}:${minutes} ${amOrPm}`;
   };
 
   const formatFileSize = (bytes) => {
@@ -350,7 +351,7 @@ const Keuring = ({ params }) => {
             <TagLabel>
               {keuring.status +
                 (keuring.status == Status.INGEPLAND
-                  ? " -> " + formatDate(keuring.datumPlaatsbezoek)
+                  ? ": " + formatDate(keuring.datumPlaatsbezoek)
                   : "")}
             </TagLabel>
           </Tag>
--- src/app/(dashboard)/keuringen/page.jsx
+++ src/app/(dashboard)/keuringen/page.jsx
@@ -66,10 +66,11 @@ const Keuringen = () => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
-    const hours = date.getHours();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
     const amOrPm = hours >= 12 ? "PM" : "AM";
 
-    return `${day}/${month}/${year} ${amOrPm}`;
+    return `${day}/${month}/${year} ${hours}:${minutes} ${amOrPm}`;
   };
 
   const handlePageChange = (page) => {
@@ -242,7 +243,7 @@ const Keuringen = () => {
                               <Text fontSize="small">
                                 {keuring.status +
                                   (keuring.status == Status.INGEPLAND
-                                    ? " -> " +
+                                    ? ": " +
                                       formatDate(keuring.datumPlaatsbezoek)
                                     : "")}
                               </Text>
